217db33e569efbcfd7a746413b5a326c39bba0ab
Paulina Lee <0618py@gmail.com>
2017-04-01 23:35:07 -0500

    committing before everything erupts into flames

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var time = localStorage.totalTime;
-var mins = time;
+var mins = time / count;
 var secs = mins*60;
 var count = localStorage.total;
+var list = localStorage.exercises;
 
 function getInfo() {
 	var core = document.getElementById("core").checked;
@@ -9,32 +10,61 @@ function getInfo() {
 	var arms = document.getElementById("arms").checked;
 	var legs = document.getElementById("legs").checked;
 	var back = document.getElementById("back").checked;
-	var checkBoxes = [];
-	checkBoxes.push(core);
-	checkBoxes.push(cardio);
-	checkBoxes.push(arms);
-	checkBoxes.push(legs);
-	checkBoxes.push(back);
+	var map = {};
+	map["core"] = core;
+	map["cardio"] = cardio;
+	map["arms"] = arms;
+	map["legs"] = legs;
+	map["back"] = back;
+
+	var exercises = []
 	var count = 0;
-	for (var i = 0; i < checkBoxes.length; i++) {
-		if (checkBoxes[i])
+	for (var key in map) {
+		if (map[key]) {
 			count++;
+			exercises.push(key);
+		}
 	}
+	if (exercises.length == 0) {
+		alert("Please select at least one exercise type!");
+		return; // error catching
+	}
+	localStorage.setItem("exercises", JSON.stringify(exercises));
 	process(count);
 }
 
 function process(count) {
 	localStorage.setItem("total", count);
 	var time = document.getElementById("time").value;
-	console.log("time after proc : " + time);
 	localStorage.setItem("totalTime", time);
 	window.location.href = "workoutstart.html";
 }
 
 // timer
 function countdown() {
-	console.log(time);
-	setTimeout('Decrement()',1000);
+	changeDiv(document.getElementById("workout"));
+	mins = time / localStorage.total;
+	secs = mins * 60;
+	setTimeout('Decrement()', 2000);
+}
+
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getWorkout() {
+	var list = JSON.parse(localStorage.exercises); // parsed list of exercises
+	var workoutType = list[localStorage.total - count]; // current exercise type
+	var ex = Workout.instances[workoutType]; // list of corresponding exercises
+	return ex[getRandomInt(0, ex.length)];
+}
+
+function changeDiv(div) {
+	var workout = getWorkout();
+	div.innerHTML = "<h3>" + workout.name + "</h3> <br><div style=\"font-size:15px\">"
+		 + workout.descrip + "</div>";
 }
 
 function Decrement() {
@@ -50,10 +80,10 @@ function Decrement() {
 		}
 		if(getminutes()==0&&getseconds()==0)
 			restart();
-		else
+		else {
 			setTimeout('Decrement()',1000);
-		secs--;
-
+			secs--;
+		}
 	}
 }
 function getminutes() {
@@ -68,9 +98,19 @@ function getseconds() {
 
 function restart(){
 	//alert("Hello");
-	console.log("DONE");
+	count--;
+	if (count > 0) {
+		setTimeout('countdown()', 1000);
+		// countdown();
+	}
 }
 
+function start() {
+	if (core.length == 0)
+		Workout.data(); // init
+	document.getElementById("start").disabled = true;
+	countdown();
+}
 
 // workouts
 Workout.instances = {};
@@ -93,7 +133,7 @@ Workout.data = function() {
 	cardio.push(new Workout({name:"Jumping Jacks", area:"cardio", descrip:"Stand normally, then jump your feet out to the sides and raise your arms over your head before returning to the starting position."}));
 	arms.push(new Workout({name:"Tricep Dips", area:"arms", descrip:"Place your hands shoulder-width apart on a stable surface. Hold yourself with your arms straight and your legs extended in front of you. Slowly lower your arms into a 90-degree angle, and then straighten."}));
 	legs.push(new Workout({name:"Squats", area:"legs", descrip:"Stand with your feet apart, look straight ahead and hold your arms straight out in front of you. Lower your hips into a squat, keeping your knees over your feet and your back straight."}));
-	legs.push(new Workout({name:"Lunges", area:"legs", descrip:"Step forward with one foot at a time, lowering until your knees are both at 90-degree angles. Make sure your front knee stays over your foot and your back knee doesn’t touch the ground."}));
+	legs.push(new Workout({name:"Lunges", area:"legs", descrip:"Step forward with one foot at a time, lowering until your knees are both at 90-degree angles. Make sure your front knee stays over your foot and your back knee doesn't touch the ground."}));
 	core.push(new Workout({name:"Russian Twists", area:"core", descrip:"Sit on the ground with your knees bent. Your upper body should create a V-shape with your thighs. Extend your arms out fully in front of you, and then turn to each side until they’re parallel to the ground."}));
 	legs.push(new Workout({name:"Calf Raises", area:"legs", descrip:"Stand on the edge of a step or something elevated, with your heels hanging off the back and your toes on the ground. Raise your heels so you’re on tiptoe, and then lower them so they’re below the edge of the platform."}));
 	arms.push(new Workout({name:"Push Ups", area:"arms", descrip:"Get into a plank position. Lower your body so that your chest gets as close to the ground as possible. Pull your elbows back as you descend, and keep your body straight."}));
@@ -106,8 +146,8 @@ Workout.data = function() {
 	cardio.push(new Workout({name:"Butt Kickers", area:"cardio", descrip:"Stand normally, and then run in place, bringing your heels up to your butt each time."}));
 	arms.push(new Workout({name:"Small Arm Circles", area:"arms", descrip:"Extend your arms straight out to your sides. Move them in small, quick circles."}));
 	arms.push(new Workout({name:"Big Arm Circles", area:"arms", descrip:"Extend your arms straight out to your sides. Move them in large, wide circles."}));
-	arms.push(new Workout({name:"Plank Ups", area:"arms", descrip:"Start in a high plank position. Lower one forearm to the ground, then the other, until you’re in a low plank. Then straighten one arm at a time into a high plank."}));
-	back.push(new Workout({name:"Supermans", area:"back", descrip:"Lie facedown on the ground. Reach your arms and legs straight out into the air so it looks like you’re doing a superman pose, and then lower."}));
+	arms.push(new Workout({name:"Plank Ups", area:"arms", descrip:"Start in a high plank position. Lower one forearm to the ground, then the other, until you're in a low plank. Then straighten one arm at a time into a high plank."}));
+	back.push(new Workout({name:"Supermans", area:"back", descrip:"Lie facedown on the ground. Reach your arms and legs straight out into the air so it looks like you're doing a superman pose, and then lower."}));
 	back.push(new Workout({name:"Bridge", area:"back", descrip:"Lay on your back with your legs bent at the knees. Lift your pelvis up to the ceiling and then lower it again."}));
 	Workout.instances["arms"] = arms;
 	Workout.instances["core"] = core;
